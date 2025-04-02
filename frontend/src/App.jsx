b62230d05ad8e60579aadc20b443d7b97a3f46d9
hajirah84 <mianhajirah@gmail.com>
2025-04-01 20:22:56 -0400

    added top navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import TopicList from './components/TopicList';
+import TopNavigationBar from './components/TopNavigationBar';
 import PhotoList from './components/PhotoList';
 import './App.scss';
 
-const App = () => {
-  return (
-    <div className="App">
-      <TopicList />
-      <PhotoList />
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <TopNavigationBar />
+    <PhotoList />
+  </div>
+);
 
 export default App;
+
 
 
 
